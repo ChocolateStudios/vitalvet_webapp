430ff3ea4273836cc61c73cc22d6ff213cc38f99
DiegoAlonso1 <diegoalonsooliverab@gmail.com>
2025-11-28 23:28:38 -0500

    feat: PetMiniprofileCard component was cleaned

---
+++ src/contexts/pets/client/presentation/components/pet-miniprofile-card.constants.ts
@@ -0,0 +1,26 @@
+
+/**
+ * Maps the colSpan prop to Tailwind CSS classes for grid column spanning.
+ */
+export const colSpanClasses = {
+    "1": "col-span-1",
+    "2": "col-span-2",
+    "3": "col-span-3",
+    "4": "col-span-4",
+    "5": "col-span-5",
+    "6": "col-span-6",
+    full: "col-span-full",
+};
+
+/**
+ * Maps the lgColSpan prop to Tailwind CSS classes for grid column spanning on large screens.
+ */
+export const lgColSpanClasses = {
+    "1": "lg:col-span-1",
+    "2": "lg:col-span-2",
+    "3": "lg:col-span-3",
+    "4": "lg:col-span-4",
+    "5": "lg:col-span-5",
+    "6": "lg:col-span-6",
+    full: "lg:col-span-full",
+};
